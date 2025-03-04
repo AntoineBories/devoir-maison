312edd389ef53fd86b22bc2818caccde78bcff96
AntoineBories <antoine.bories@gmail.com>
2025-03-04 12:53:48 +0100

    ex 1 et 2

---
+++ code.py
@@ -0,0 +1,6 @@
+#ici pour le code 
+# voie a suivre : 
+# - ouvrir le fichier germinal_nettoye.txt, 
+# - ajouter chaque mot a un dictionnaire avec comme cle le mot et comme valeur le nombre de fois ou il est apparu 
+# - comparer le dictionnaire avec le les fichiers "cl_...."
+# - écrire le nom du fichier auquel il y a le plus de similitude 
